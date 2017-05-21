f486324b2587a752cfe9cbd79bdde64fee31af60
jekstrom <james.ekstrom@gmail.com>
2017-05-20 20:45:11 -0400

    Add letsencrypt nonce.

---
+++ weblog/Controllers/LetsEncryptController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace weblog.Controllers
+{
+	public class LetsEncryptController : Controller
+	{
+		[HttpGet]
+		[Route(".well-known/acme-challenge/UFC70B-R9iV1vDOMG2SJJ94Bn18KAitU-zgsnot853A")]
+		public IActionResult Verify()
+		{
+			string nonce = "UFC70B-R9iV1vDOMG2SJJ94Bn18KAitU-zgsnot853A.5zRqNqFhk-Ia6CwtsCtB9_TxvElwheqNigRuXxqyEO8";
+			return new ObjectResult(nonce);
+		}
+	}
+}
