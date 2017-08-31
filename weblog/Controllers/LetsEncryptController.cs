c3e93f8895c8cf2aa411075fd360fa9d8be4dc5e
James Ekstrom <james.ekstrom@arinet.com>
2017-08-31 16:29:34 -0500

    Update LE challenge nonce.

--- weblog/Controllers/LetsEncryptController.cs
+++ weblog/Controllers/LetsEncryptController.cs
@@ -9,7 +9,7 @@ namespace weblog.Controllers
 	public class LetsEncryptController : Controller
 	{
 		[HttpGet]
-		[Route(".well-known/acme-challenge/dIPPI-VdxTwJh91ZxfbfYn927ozJlqbp3Q6BLxM_iP4]")]
+		[Route(".well-known/acme-challenge/dIPPI-VdxTwJh91ZxfbfYn927ozJlqbp3Q6BLxM_iP4")]
 		public IActionResult Verify()
 		{
 			string nonce = "dIPPI-VdxTwJh91ZxfbfYn927ozJlqbp3Q6BLxM_iP4.6kqZFPlL1G73MD7rpngpKy3uksSfZQUojbI-XsG2Tp8";
